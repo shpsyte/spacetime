e7bf184ddeff0decad220f0b6d55d60dbd799ffe
luiz <jose.iscosistemas@gmail.com>
2023-05-17 15:23:35 -0700

    React-Native: Styling Home Page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,6 +8,7 @@ import {
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 import Stripes from './src/assets/stripes.svg'
+import Logo from './src/assets/logo.svg'
 import { styled } from 'nativewind'
 
 const StyledStripes = styled(Stripes)
@@ -24,13 +25,27 @@ export default function App() {
   return (
     <ImageBackground
       source={blurBg}
-      className="relative flex-1 items-center bg-gray-900"
+      className="relative flex-1 items-center bg-gray-900 px-8"
       imageStyle={{
         position: 'absolute',
         left: '-100%',
       }}
     >
       <StyledStripes className="absolute left-0" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <Logo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+      </View>
+
       {/* <Text className="font-title text-5xl text-gray-50">Rocketseat</Text> */}
       <StatusBar style="light" translucent />
     </ImageBackground>
