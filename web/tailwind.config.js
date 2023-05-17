2030c18432c25fcab1f6dca023db45dcebce4c03
luiz <jose.iscosistemas@gmail.com>
2023-05-17 11:11:53 -0700

    creating stripes with css

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -80,6 +80,11 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        stripes:
+          'linear-gradient(to bottom, rgba(255,255,255,0.1), rgba(255,255,255,0.1) 12.5%, transparent 12.5%, transparent )',
+      },
+      backgroundSize: {
+        stripes: '100% 8px',
       },
     },
   },
