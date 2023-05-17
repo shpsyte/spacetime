5a0d5d598698bd8393166821085311166441cc89
luiz <jose.iscosistemas@gmail.com>
2023-05-17 10:11:30 -0700

    feat: setting up google font on next/web project by next-fonts

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <>
-      <div className="text-5xl uppercase text-green-700">oi</div>
+      <div className="text-5xl uppercase text-green-700">
+        Esta e um exemplo de font
+      </div>
+      <div className="font-alt text-5xl uppercase text-green-950">
+        Esta e um exemplo de font
+      </div>
     </>
   )
 }
