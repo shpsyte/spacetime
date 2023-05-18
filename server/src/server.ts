f0d5d829bebf410a42b68f4e0c4bac95d19cb965
luiz <jose.iscosistemas@gmail.com>
2023-05-18 13:09:34 -0700

    Server: Emitindo um JWT quando registrado um user

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 import { authRoute } from './routes/auth'
+import jwt from '@fastify/jwt'
 
 const app = fastify()
 app.register(cors, {
@@ -11,6 +12,9 @@ app.register(cors, {
   // ['http://localhost:3000', 'http://localhost:3001'],
 
   // put your options here
+})
+app.register(jwt, {
+  secret: 'space-2f095052-d782-437e-b69d-0239a8ff1e52',
 })
 
 app.register(memoriesRoutes)
