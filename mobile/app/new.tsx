f0a7820855c69d1d41eba8296d2d7e2a96495cfb
luiz <jose.iscosistemas@gmail.com>
2023-05-19 13:48:20 -0700

    React Native: salvando dados no baco

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -7,18 +7,21 @@ import {
   View,
 } from 'react-native'
 import Logo from '../src/assets/logo.svg'
-import { Link } from 'expo-router'
+import { Link, useRouter } from 'expo-router'
 import Icon from '@expo/vector-icons/Feather'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useState } from 'react'
 import * as ImagePicker from 'expo-image-picker'
 import { Image } from 'react-native'
+import * as SecureStore from 'expo-secure-store'
+import { api } from '../src/lib/api'
 
 export default function Memories() {
   const [isPublic, setIsPublic] = useState(false)
   const [content, setContent] = useState('')
   const [preview, setPreview] = useState<string | null>('')
   const { bottom, top } = useSafeAreaInsets()
+  const router = useRouter()
 
   async function openImagPicker() {
     try {
@@ -34,11 +37,43 @@ export default function Memories() {
       }
     } catch (error) {}
   }
-  function handleCreateMemory() {
-    console.log('create memory', {
-      isPublic,
-      content,
-    })
+  async function handleCreateMemory() {
+    const token = await SecureStore.getItemAsync('token')
+
+    let coverUrl = ''
+
+    if (preview) {
+      const uploadFormdata = new FormData()
+      uploadFormdata.append('file', {
+        name: 'cover.jpg',
+        type: 'image/jpg',
+        uri: preview,
+      } as any)
+
+      const updatRespondse = await api.post('/upload', uploadFormdata, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+
+      coverUrl = updatRespondse.data.fileUrl
+    }
+
+    const response = await api.post(
+      '/memories',
+      {
+        content,
+        isPublic,
+        coverUrl,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+
+    router.push('/memories')
   }
 
   return (
@@ -109,7 +144,6 @@ export default function Memories() {
       >
         <Text className="font-alt text-sm uppercase text-black">Salvar</Text>
       </TouchableOpacity>
-      {/* <Text>New Memories</Text> */}
     </ScrollView>
   )
 }
