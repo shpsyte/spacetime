ee19994d355acffba79a13424147c5b2585d1174
luiz <jose.iscosistemas@gmail.com>
2023-05-19 09:40:22 -0700

    Web: Estilizando a pagina de nova memoria

--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -1,5 +1,52 @@
+import { Camera, ChevronLeft } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 export default function NewMomory() {
-  return <div>page</div>
+  return (
+    <>
+      <div className="flex flex-1 flex-col gap-4">
+        <Link
+          href="/"
+          className="flex items-center gap-1 text-sm text-gray-200 hover:text-gray-100"
+        >
+          <ChevronLeft className="h4 w-4" />
+          Voltar
+        </Link>
+
+        <form className="flex flex-1 flex-col gap-2" action="">
+          <div className="flex items-center gap-4">
+            <label
+              htmlFor="media"
+              className="flex cursor-pointer items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
+            >
+              <Camera className="h4 w-4" />
+              Anexar Midia
+            </label>
+            <label
+              htmlFor="isPublic"
+              className="flex items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
+            >
+              Publica?
+            </label>
+            <input
+              type="checkbox"
+              name="isPublic"
+              id="isPublic"
+              value="true"
+              className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500 "
+            />
+            Tornar publico
+          </div>
+          <input type="file" id="media" className="invisible h-0 w-0" />
+          <textarea
+            name="content"
+            spellCheck={false}
+            className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
+            placeholder="Fique livre para adicionar sua memoria aqui..."
+          />
+        </form>
+      </div>
+    </>
+  )
 }
